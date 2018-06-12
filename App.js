8d9a60f2a764226f24b18419eeababd4c9e7be0b
Elisandro <e.s.tech@outlook.com>
2018-06-12 16:09:00 -0300

    App de Lista de Contatos!

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StackNavigator } from 'react-navigation';
+
+import PeoplePage from './src/pages/PeoplePage';
+import PeopleDetailPage from './src/pages/PeopleDetailPage';
+
+import { capitalizeFirstLetter } from './src/util';
+
+export default StackNavigator({
+  'Main': {
+    screen: PeoplePage,
+  },
+  'PeopleDetail': {
+    screen: PeopleDetailPage,
+    navigationOptions: ({ navigation }) => {
+      const peopleName = capitalizeFirstLetter
+      (
+        navigation.state.params.people.name.first
+      );
+      return({
+        title: peopleName,
+        headerTitleStyle: {
+        color: "#fff",
+        fontSize: 30
+      }
+      })
+    }
+  }
+}, {
+  navigationOptions: {
+    title: 'Pessoas!',
+    headerTintColor: '#fff',
+    headerStyle: {
+      backgroundColor: '#6ca2f7',
+      borderBottomWidth: 1,
+      borderBottomColor: "#C5C5C5ff",
+    },
+    titleStyle: {
+      color: "#fff",
+      fontSize: 30,
+      flexDirection: 'row',
+      justifyContent: 'flex-end'
+    }
+  }
+});
